d259809cd3ec8fb3d9a828c8182048f5748bc3b6
Franco Risso <franco@720desarrollos.com>
2015-06-20 19:07:44 -0300

    first commit

---
+++ project.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
+app = Flask(__name__)
+
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+
